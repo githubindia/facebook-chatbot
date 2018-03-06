b83055032f5d1ca3f0ff299ca4687c2aa994dc18
AbhishekDash-Hexaware <shubham08gupta08@gmail.com>
2018-03-06 15:12:38 +0530

    desync package added

--- api/src/processMessage.js
+++ api/src/processMessage.js
@@ -1,10 +1,12 @@
 const request = require('request');
+var deasync=require('deasync');
 var schema = require('./schema.json');
 var natural = require('natural');
 var calc = require('./module.js');
 const FACEBOOK_ACCESS_TOKEN = "EAAc6hI7VvPwBANp3BZAgcztmOwZCUmHrhBvUi0EZAOn2Byhq6i2jjiqdIHUcNZBZADgj5HZAXpCzl962Nhi6ou23blwFOZCQosStTY3tGPZAzCxGkQAPgOdpjHSMxIqK5f2mUq8ovFZCveHwZAVbF6ZB4yqjlZCZCCpWvdnf0iTnbrUizmQZDZD";
 var servicenow = require('./servicenow.js');
 var async = require('async');
+var process = require('./process.js')
 
 module.exports = (event) => {
     const senderId = event.sender.id;
@@ -12,10 +14,9 @@ module.exports = (event) => {
     var response = [];
     var flag = false;
     var result = [];
-    var schemaResponse;
     var contextFlag = false;
     var reply;
-    var resp = [];
+    var resp ;
     var out;
 
 if(schema.intentSchema[1].context) {
@@ -23,7 +24,6 @@ if(schema.intentSchema[1].context) {
         element.utterances.forEach(function(element1, j) {
             //console.log("Score : "+natural.JaroWinklerDistance(message, element1));
             if (natural.JaroWinklerDistance(message, element1) >= 0.75) {
-                schemaResponse = element;
                 response.push(element.responses[Math.floor(Math.random() * Math.floor(j))]);
                 //console.log(element.context);
                 if(i == 1) {
@@ -32,71 +32,31 @@ if(schema.intentSchema[1].context) {
             }
         });
     });
+} else {
+
+    let serviceNowResponse = deasync(function(callback){
+        servicenow.logIncident(message, callback);
+    })();
+    //console.log("Service Now Response Obtained Outside Callback : "+serviceNowResponse.result.number)
+    response.push(`Service Now Response Obtained Outside Callback : ${serviceNowResponse.result.number}`);
+    schema.intentSchema[1].context = true;
 }
 
-    
-    //  else  if (response.length == 0) {
-    //     calc.regexCalc(message, function(res, type) {
-    //         //response = res;
-    //     });
-    // }
     if (response.length != 0) {
         reply = response[Math.floor(Math.random())];
-        out = request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: FACEBOOK_ACCESS_TOKEN },
-        method: 'POST',
-        json: {
-            recipient: { id: senderId },
-            "message": {
-                "text" : reply
+        var out = request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+            method: 'POST',
+            json: {
+                recipient: { id: senderId },
+                "message": {
+                    "text" : reply
+                }
             }
-        }
-    });
-    out.on('error', function(err) {
-            console.log("Timedout error");
-            console.log(err.message.code);
-    });
-    } else {
-        if (response.length == 0) {
-            if (!schema.intentSchema[1].context) {
-                servicenow.logIncident(message, function(err, body) {
-                    resp.push(`Your incident has been created with the incident number`); //${body.result.number}
-                    //console.log(reply);
-                    reply = resp[0];
-                });
-                schema.intentSchema[1].context = true;
-                out = request({
-                    url: 'https://graph.facebook.com/v2.6/me/messages',
-                    qs: { access_token: FACEBOOK_ACCESS_TOKEN },
-                    method: 'POST',
-                    json: {
-                        recipient: { id: senderId },
-                        "message": {
-                            "text" : reply
-                        }
-                    }
-                });
-                out.on('error', function(err) {
-                        console.log("Timedout error");
-                });
-            } else {
-                reply = "Sorry! I didn't get that";
-            }
-        }
+        });
+        out.on('error', function(err) {
+                console.log("Timedout error");
+        });
     }
-
-    // if(contextFlag) {
-    //     reply = resp[0];
-    //     console.log(reply);
-        
-    // }
-
-
-    
-    // result.push({id: global.followUpSymptomId, choice_id: global.diagnosisSymptomStatus});
-
-    
-
-    
 };
